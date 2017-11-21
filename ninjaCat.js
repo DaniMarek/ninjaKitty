adc67d9d713b3d0eefccdb9da337ea06d3d3906d
DaniMarek <Dramenika@outlook.com>
2017-11-21 08:06:05 -0600

    ninjaCat.css and ninjaCat.js added. An previous error did not allow initial upload.

---
+++ ninjaCat.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	// console.log('test');
+ 	$("img").click(function() {
+ 		var temp = $(this).attr('data-alt-src');
+ 		var src = $(this).attr('src');
+ 		console.log('data-alt-src value is', temp);
+ 		
+ 		$(this).attr('data-alt-src', src)
+ 		// use .attr() as a setter to set the 'src' to altSrc
+
+ 		$(this).attr("src", temp);
+ 		// $(this).attr(altSrc, "src");
+	});
+
+});
